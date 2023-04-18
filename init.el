334f6edd007f664810a48be81eb6626013811056
alo40 <alejandronieto40@gmail.com>
2023-04-18 20:39:38 +0200

    added init.el and basic config

---
+++ init.el
@@ -0,0 +1,20 @@
+;; My EMACS config
+
+;; don't show the splash screen
+(setq inhibit-startup-message t)
+
+;; ;; flahses when activated (only for testing)
+;; (setq visible-bell t)
+
+;; some configurations
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
+;; display line numbers
+(global-display-line-numbers-mode 1)
+
+;; load color theme
+;; (load-theme 'deeper-blue nil)
+ (load-theme 'misterioso nil)
+
